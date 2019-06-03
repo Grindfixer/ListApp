553df5de500285f9748bef2f814b5083e3fca3fa
jwn812 <john.nicholson812@gmail.com>
2019-06-03 13:10:54 -0400

    Initial commit

---
+++ app/src/main/java/android/jwn/listapp/MainActivity.java
@@ -0,0 +1,31 @@
+package android.jwn.listapp;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView appListView;//reference appListView in activity_main.xml
+    String[] items;//reference string-array named "items" in strings.xml
+    String[] prices;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        appListView = (ListView)findViewById(R.id.appListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        appListView.setAdapter(itemAdapter);
+
+    }
+}
